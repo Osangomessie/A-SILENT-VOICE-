8cc47fc2f23ae8c86affb6ae6788fd61d5d7b6f4
Osangomessie <messieosango659@gmail.com>
2025-02-02 23:41:35 +0100

    Update help.js
    Signed-off-by: Osangomessie <messieosango659@gmail.com>

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 𝑀𝐸𝑆𝑆𝐼𝐸 𝑂𝑆𝐴𝑁𝐺𝑂 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
+const doNotDelete = "[ 𝑀𝐸𝑆𝑆𝐼𝐸 𝕆𝐒𝐀𝐍𝐆𝐎 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
 
 module.exports = {
   config: {
@@ -117,4 +117,4 @@ function roleTextToString(roleText) {
     default:
       return "Unknown role";
   }
-    }
+}
